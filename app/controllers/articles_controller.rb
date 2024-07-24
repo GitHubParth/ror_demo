feea4574c0238fb8dbdbd8a658b21988effc1aaa
GitHubParth <parthpadhiar2002@gmail.com>
2024-07-24 12:54:13 +0000

    Learned CRUD in Rails.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -79,5 +79,39 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article created successfully"
+      redirect_to @article
+    else 
+      render 'new', status: 422
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit', status: 422
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
 end
