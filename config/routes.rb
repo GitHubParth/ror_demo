bbb0449573e7b9cf2c09365d80048dd997071560
GitHubParth <parthpadhiar2002@gmail.com>
2024-07-25 10:11:57 +0000

    Added users db

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
   resources :articles
+  resources :users, only: [:index]
+
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'services', to: 'pages#services'
