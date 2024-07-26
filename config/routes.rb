8228906987e1dce18ab7704e869676213d290478
GitHubParth <parthpadhiar2002@gmail.com>
2024-07-26 05:45:44 +0000

    Adding Users Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
   resources :articles
-  resources :users, only: [:index]
+  resources :users, except: [:new]
 
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'services', to: 'pages#services'
   get 'contact', to: 'pages#contact'
+  get 'signup', to: 'users#new'
   
 end
